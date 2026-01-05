2046a072b48bda04e24b10ea64a68951ad77b1ee
Carlos Rivera Fernandez <Carlos.Rivera@qaracter.com>
2026-01-05 12:39:28 +0100

    fix: AlertService message changed

--- src/main/java/org/example/transactions/AlertService.java
+++ src/main/java/org/example/transactions/AlertService.java
@@ -2,6 +2,6 @@ package org.example.transactions;
 
 public class AlertService {
     public static void sendLongBalanceAlert(long accountId) {
-        System.out.println("Sending long balance alert for account " + accountId + ".");
+        System.out.println("Long balance alert correctly sent for account " + accountId + ".");
     }
 }
